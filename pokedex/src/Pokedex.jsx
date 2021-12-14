ec247035ad47a62eed4570d632bfa83ab582caf4
Christian Bouwense <bouwensec@gmail.com>
2021-12-14 15:04:12 -0500

    cleaned up the prod code a little.

--- pokedex/src/Pokedex.jsx
+++ pokedex/src/Pokedex.jsx
@@ -1,23 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { getPokemon } from "./adapters/storageAdapter";
 
-const usePokedex = () => {
+export const Pokedex = () => {
    const [ pokemon, setPokemon ] = useState();
    
    useEffect(() => {
-      console.log("in useEffect")
       async function getAndSetPokemon() {
          setPokemon(await getPokemon());
       }
       getAndSetPokemon();
-      console.log("after getAndSetPokemon")
    }, []);
-
-   return { pokemon };
-};
-
-export const Pokedex = () => {
-   const { pokemon } = usePokedex();
 
    if (!pokemon) return <p role="status">loading...</p>;
    return <p>done!</p>;
