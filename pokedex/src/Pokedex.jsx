b1e5a3b35700f7d24b9fdb7beacef86b6c290686
Christian Bouwense <bouwensec@gmail.com>
2021-12-14 17:19:10 -0500

    made solid tests for the first three reqs I think

--- pokedex/src/Pokedex.jsx
+++ pokedex/src/Pokedex.jsx
@@ -6,7 +6,7 @@ export const Pokedex = () => {
    
    useEffect(() => {
       async function getAndSetPokemon() {
-         setPokemon(await getPokemon());
+         getPokemon().then(setPokemon);
       }
       getAndSetPokemon();
    }, []);
--- pokedex/src/Pokedex.test.jsx
+++ pokedex/src/Pokedex.test.jsx
@@ -1,28 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import { Pokedex } from "./Pokedex";
 import { getPokemon } from "./adapters/storageAdapter";
 
 jest.mock("./adapters/storageAdapter");
 
 describe("given the pokemon data is not available to the client", () => {
-   let loadingStatus;
-   
    describe("when the pokemon have yet to become available to the client", () => {
       beforeEach(() => {
-         render(<Pokedex />);
-         loadingStatus = screen.getByRole("status");
+         getPokemon.mockResolvedValue("mock res");
       });
       
       it("should render a status indicating that the page is loading pokemon", async () => {
-         expect(loadingStatus).toBeInTheDocument();
+         render(<Pokedex />);
+         expect(screen.getByRole("status")).toBeInTheDocument();
+         await waitForElementToBeRemoved(() => screen.getByRole("status"));
       });
 
-      it("should render that status with the text \"loading...\"", () => {
-         expect(loadingStatus.textContent).toBe("loading...");
+      it("should render that status with the text \"loading...\"", async () => {
+         render(<Pokedex />);
+         expect(screen.getByRole("status").textContent).toBe("loading...");
+         await waitForElementToBeRemoved(() => screen.getByRole("status"));
       });
 
       it("should request the pokemon once", async () => {
+         render(<Pokedex />);
          expect(getPokemon).toHaveBeenCalledTimes(1);
+         await waitForElementToBeRemoved(() => screen.getByRole("status"));
       });
    });
 });
