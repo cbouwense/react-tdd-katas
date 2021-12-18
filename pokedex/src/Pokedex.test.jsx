624e21ec66ad2cc1c242c7033c6a1e8d3568b99f
Christian Bouwense <bouwensec@gmail.com>
2021-12-18 14:12:53 -0500

    abstracted server use cases to msw config file.

--- pokedex/src/Pokedex.test.jsx
+++ pokedex/src/Pokedex.test.jsx
@@ -1,10 +1,7 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { Pokedex } from "./Pokedex";
 import * as adapters from "./adapters/storageAdapter";
-import { server } from "./mockServiceWorker";
-import { rest } from "msw";
-
-let getPokemonSpy;
+import { makeServerReturnAStatusOf } from "./mockServiceWorker";
 
 afterEach(async () => {
    jest.restoreAllMocks();
@@ -13,50 +10,56 @@ afterEach(async () => {
 describe("when the pokemon have yet to become available to the client", () => {     
    it("should render some status text", () => {
       render(<Pokedex />);
+
       expect(screen.getByRole("status")).toBeInTheDocument();
    });
 
    it("should render some status text saying \"loading...\"", () => {
       render(<Pokedex />);
+
       expect(screen.getByRole("status").textContent).toBe("loading...");
    });
 
    it("should make one attempt to get the pokemon data", () => {
-      getPokemonSpy = jest.spyOn(adapters, "getPokemon");
+      const getPokemonSpy = jest.spyOn(adapters, "getPokemon");
+
       render(<Pokedex />);
+
       expect(getPokemonSpy).toHaveBeenCalledTimes(1);
    });
 });
 
 describe("given we have sent off the request for the pokemon", () => {      
    describe("when there is a 400 error status", () => {         
-      it("should display a status with the error message", async () => {
-         server.use(rest.get("https://pokeapi.co/api/v2/pokemon/", (_, res, ctx) => {
-            return res(ctx.status(404));
-         }));
+      it("should display a status with the error message", async () => {   
+         makeServerReturnAStatusOf(404);
+         
          render(<Pokedex />);
+
          await waitFor(() => expect(screen.getByRole("status")).toHaveTextContent("Not Found"));
       });
    });
    
-   describe("when there is a 500 error status", () => {         
+   describe("when there is a 500 error status", () => {        
       it("should display a status with the error message", async () => {
-         server.use(rest.get("https://pokeapi.co/api/v2/pokemon/", (_, res, ctx) => {
-            return res(ctx.status(500));
-         }));
+         makeServerReturnAStatusOf(500);
+         
          render(<Pokedex />);
+
          await waitFor(() => expect(screen.getByRole("status")).toHaveTextContent("Internal Server Error"));
       });
    });
 
-   describe("when is a valid response", () => {         
+   describe("when there is a valid response", () => {         
       it("should display a list to contain the pokemon", async () => {
          render(<Pokedex />);
+
          await waitFor(() => expect(screen.getByRole("list")).toBeInTheDocument());
       });
 
       it("should display one card for each pokemon in that list", async () => {
          render(<Pokedex />);
+         
          await waitFor(() => expect(screen.getAllByRole("listitem")).toHaveLength(3));
       });
    });
--- pokedex/src/mockServiceWorker.js
+++ pokedex/src/mockServiceWorker.js
@@ -1,28 +1,36 @@
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 
+export const mockResponseData = {
+   successWithThreePokemon: {
+      "count": 1118,
+      "next": "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+      "previous": null,
+      "results": [
+          {
+              "name": "bulbasaur",
+              "url": "https://pokeapi.co/api/v2/pokemon/1/"
+          },
+          {
+              "name": "ivysaur",
+              "url": "https://pokeapi.co/api/v2/pokemon/2/"
+          },
+          {
+              "name": "venusaur",
+              "url": "https://pokeapi.co/api/v2/pokemon/3/"
+          },
+      ]
+  }
+};
+
 export const server = setupServer(
    rest.get("https://pokeapi.co/api/v2/pokemon/", (_, res, ctx) => {
-      return res(ctx.json(
-         {
-            "count": 1118,
-            "next": "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
-            "previous": null,
-            "results": [
-                {
-                    "name": "bulbasaur",
-                    "url": "https://pokeapi.co/api/v2/pokemon/1/"
-                },
-                {
-                    "name": "ivysaur",
-                    "url": "https://pokeapi.co/api/v2/pokemon/2/"
-                },
-                {
-                    "name": "venusaur",
-                    "url": "https://pokeapi.co/api/v2/pokemon/3/"
-                },
-            ]
-        }
-      ));
+      return res(ctx.json(mockResponseData.successWithThreePokemon));
    }),
 );
+
+export const makeServerReturnAStatusOf = (statusCode) => {
+   server.use(rest.get("https://pokeapi.co/api/v2/pokemon/", (_, res, ctx) => {
+      return res(ctx.status(statusCode));
+   }));
+};
