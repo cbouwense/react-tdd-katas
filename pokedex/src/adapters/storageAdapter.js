bec10f40cc7f012802f3619da44670a9d1ab84f4
Christian Bouwense <bouwensec@gmail.com>
2021-12-18 22:14:12 -0500

    made storage adapter cleaner.

--- pokedex/src/adapters/storageAdapter.js
+++ pokedex/src/adapters/storageAdapter.js
@@ -1,13 +1,23 @@
+const specificInfoForEachPokemon = (listOfPokemon) =>
+   Promise.all(
+      listOfPokemon.map(
+         async (p) => {
+            const res = await fetch(p.url);
+            const { id, name } = await res.json();
+            return { id, name };
+         }
+      )
+   );
+
+const listOfPokemonInResponse = async (response) => {
+   const pokemonMetadata = await response.json();
+   return pokemonMetadata.results;
+};
+
 export const getPokemon = async () => {
-   const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
-   if (!res.ok) throw res.statusText;
+   const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+   if (!response.ok) throw response.statusText;
 
-   const pokemonMetadata = await res.json();
-   const pokemon = pokemonMetadata.results;
-
-   return Promise.all(pokemon.map(async (p) => {
-      const res = await fetch(p.url);
-      const { id, name } = await res.json();
-      return { id, name };
-   }));
+   const pokemonList = await listOfPokemonInResponse(response);
+   return specificInfoForEachPokemon(pokemonList);
 };
